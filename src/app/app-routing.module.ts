57044c1b231e7a695bcc35d3a3b9a0cbeda90c3d
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2023-02-19 18:27:28 +0100

    Add route for cart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
