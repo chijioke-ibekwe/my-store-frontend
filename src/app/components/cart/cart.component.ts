ec9e09e40e302c4f4b882d7fc1d5a5e298a5fec1
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2023-02-25 14:14:10 +0100

    Implement cart item details two way binding

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CartItem } from 'src/app/models/CartItem';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -7,16 +7,44 @@ import { CartService } from 'src/app/services/cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit{
+export class CartComponent implements OnInit, OnChanges{
   cartItems: CartItem[] = [];
+  fullName: string = '';
+  amountDue: string = '0';
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.cartItems = this.cartService.getCartItems();
+    this.amountDue = this.getCartItemsCost(this.cartItems).toFixed(2);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.amountDue = this.getCartItemsCost(this.cartItems).toFixed(2);
   }
 
   quantityChange(cartItem: CartItem): void {
-    alert(`Quantity changed to ${cartItem.quantity}`);
+    if(cartItem.quantity < 1) {
+      this.cartService.removeFromCart(cartItem);
+      this.cartItems = this.cartItems.filter(item => item.id != cartItem.id);
+    }
+
+    this.amountDue = this.getCartItemsCost(this.cartItems).toFixed(2);
   }
+
+  submitForm(){
+
+  }
+
+  private getCartItemsCost(cartItems: CartItem[]): number {
+    let sum: number = 0;
+
+    cartItems.forEach((item) => {
+      const itemCost = item.price * item.quantity;
+      sum += itemCost;
+    })
+
+    return sum;
+  }
+  
 }
